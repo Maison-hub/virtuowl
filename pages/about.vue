dce0382443f3778cdde5faab84cfcd0d975dde0d
Antoine Rubeo-lisa <arotakerpro@gmail.com>
2024-04-19 11:32:07 +0200

    aboute page

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,55 @@
 <template>
+    <navbar/>
 
-    <div>
-        page2
+    <div class="w100 p-relative toppart">
+                <div class="title stroke p-absolute">
+                    A propos de nous
+                </div>
+                <h1 class="title">
+                    A propos de nous                
+                </h1>
     </div>
+    <section class="histoire">
+        <h2>NOTRE HISTOIRE</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur. Purus auctor etiam fringilla consectetur. Pulvinar tincidunt consequat magnis a. Sem non et feugiat amet at bibendum. Morbi aliquet eu pellentesque est id diam. orem ipsum dolor sit amet consectetur. Purus auctor etiam fringilla consectetur. Pulvinar tincidunt consequat magnis a. Sem non et feugiat amet at bibendum. Morbi aliquet eu pellentesque est id diam.orem ipsum dolor sit amet consectetur. Purus auctor etiam fringilla consectetur. Pulvinar tincidunt consequat magnis a. Sem non et feugiat amet at bibendum. Morbi aliquet eu pellentesque est id diam.orem ipsum dolor sit amet consectetur. Purus auctor etiam fringilla consectetur. Pulvinar tincidunt consequat magnis a. Sem non et feugiat amet at bibendum. Morbi aliquet eu pellentesque est id diam.orem ipsum dolor sit amet consectetur. Purus auctor etiam fringilla consectetur. Pulvinar tincidunt consequat magnis a. Sem non et feugiat amet at bibendum. Morbi aliquet eu pellentesque est id diam.orem ipsum dolor sit amet consectetur. Purus auctor etiam fringilla consectetur. Pulvinar tincidunt consequat magnis a. Sem non et feugiat amet at bibendum. Morbi aliquet eu pellentesque est id diam.orem ipsum dolor sit amet consectetur. Purus auctor etiam fringilla consectetur. Pulvinar tincidunt consequat magnis a. Sem non et feugiat amet at bibendum. Morbi aliquet eu pellentesque est id diam.
+        </p>
+    </section>
+
+    <section class="valeurs">
+        <h2>
+            NOS VALEURS
+        </h2>
+        <div class="listVal">
+            <div class="valItem">
+            <img src="~/assets/images/fun-icon.svg" alt="cool emoji">
+            <p>fun</p>
+        </div>
+        <div class="valItem">
+            <img src="~/assets/images/search-icon.svg" alt="loupe">
+            <p>transparent</p>
+        </div>
+        <div class="valItem">
+            <img src="~/assets/images/group-icon.svg" alt="groupe">
+            <p>Engagement Client </p>
+        </div>
+        </div>
+    </section>
+
+    <section class="equipe">
+        <h2>L’équipe</h2>
+    </section>
+
+
 
 </template>
+
+<style>
+
+
+</style>
+<script>
+
+import '~/assets/css/about.css'
+
+</script>
